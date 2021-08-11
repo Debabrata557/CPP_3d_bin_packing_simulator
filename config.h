984c96d0e8ddcd4d1058ce355f60cf04f969259e
Your Name <you@example.com>
2021-08-12 03:44:43 +0530

    print structures

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #include <random>
 #include <tuple>
-
+#include <iostream>
 #ifndef config
 #define config
 const int DEBUG = 0;
@@ -15,9 +15,10 @@ const int MIN_BOX_WIDTH = 30;
 const int MIN_BOX_HEIGHT = 30;
 const int CONTROLLER_TOLERANCE = 3;
 
-const int SCALING_FACTOR = 1;  // effective bin_width/height/length = bin_width/height/length *(1/scaling_factor)..Make sure the effective bin_width/height/length are integers
+const int SCALING_FACTOR = 1; // effective bin_width/height/length = bin_width/height/length *(1/scaling_factor)..Make sure the effective bin_width/height/length are integers
 
-typedef struct vector_3d {
+typedef struct vector_3d
+{
     int x, y, z;
     // vector_3d() {
     //     x = 0, y = 0, z = 0;
@@ -29,13 +30,18 @@ typedef struct vector_3d {
     // }
 } vector_3d;
 
-typedef struct performance_metric {
+typedef struct performance_metric
+{
     double number_of_boxes_successfully_put;
     double total_number_of_boxes;
     double efficiency;
     double number_of_bins_used;
     bool exception;
 } performance_metric;
-bool operator<(const vector_3d& a, const vector_3d& b);
-bool comp_floor_building(std::pair<vector_3d, vector_3d>& a, std::pair<vector_3d, vector_3d>& b);
+bool operator<(const vector_3d &a, const vector_3d &b);
+bool comp_floor_building(std::pair<vector_3d, vector_3d> &a, std::pair<vector_3d, vector_3d> &b);
+void print_vector_3d(vector_3d &a);
+void print_list_vector_3d(std::vector<vector_3d> &a);
+void print_pair_vector_3d(std::pair<vector_3d, vector_3d> &a);
+void print_list_pair_vector_3d(std::vector<std::pair<vector_3d, vector_3d>> &a);
 #endif

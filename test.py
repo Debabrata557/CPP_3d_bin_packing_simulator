a897666d981004087d511cb1a458d202fa8b4310
Your Name <you@example.com>
2021-09-28 13:01:38 +0530

    plot curve

---
+++ test.py
@@ -0,0 +1,11 @@
+filepath="/home/achme/CPP_3d_bin_packing_simulator/test_results.txt"
+import matplotlib.pyplot as plt
+import numpy as np
+with open(filepath) as file:
+    lines = file.readlines()
+    a=[]
+    for line in lines:
+        a.append(float(line))
+    a=np.array(a)
+    plt.plot(a)
+    plt.show()
